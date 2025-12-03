fb1b8e13610bc99daab790dfffac57fc900ccbc7
Richard <rccam@csu.fullerton.edu>
2025-12-02 21:26:17 -0800

    successfully changed to PyQT5, added functionality to nav bar and adding edges to adjacency list

--- main.py
+++ main.py
@@ -1,91 +1,106 @@
-import tkinter as tk
-
-def show_frame(frame):
-    frame.tkraise()
-
-main_window = tk.Tk()
-main_window.title("Titan Campus Algorithmic Assistant")
-main_window.geometry('1280x720')
-
-main_window.rowconfigure(0, weight=1)
-main_window.columnconfigure(1, weight=1)
-
-# styling for navigation bar buttons
-button_style = {
-    "bg": "#00274c",
-    "fg": "white",
-    "font": ("Arial", 10, "bold"),
-    "relief": "flat",
-    "wraplength": 120 
-}
+import sys
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtCore import pyqtSignal, QObject, QSize
+from ui import Ui_MainWindow
 
 
-nav_frame = tk.Frame(main_window, bg="#00274c")
-nav_frame.grid(row=0, column=0, sticky = 'nsew')
+class MyApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
 
-campus_nav_btn = tk.Button(
-    nav_frame,
-    text = "CAMPUS NAVIGATION",
-    command = lambda: show_frame(campus_nav_page),
-    **button_style
-)
-campus_nav_btn.pack(fill='x', pady=10, padx=5)
+        self.setupUi(self) 
+        
+        # add icon to home button
+        buttonIcon = QIcon("resources/titans logo.png")
+        self.home_button.setIcon(buttonIcon)
+        self.home_button.clicked.connect(self.go_to_home_page)
+        self.home_button.setIconSize(QSize(130, 130))
 
-study_planner_btn = tk.Button(
-    nav_frame, 
-    text="STUDY PLANNER", 
-    command=lambda: show_frame(study_planner_page),
-    **button_style
-)
-study_planner_btn.pack(fill='x', pady=10, padx=5)
+        # set initial page to home page
+        self.stackedWidget.setCurrentWidget(self.home_page) 
 
-notes_search_btn = tk.Button(
-    nav_frame, 
-    text="NOTES SEARCH", 
-    command=lambda: show_frame(notes_search_engine_page),
-    **button_style
-)
-notes_search_btn.pack(fill='x', pady=10, padx=5)
+        # add functionality to side navbar
+        self.campus_nav_button.clicked.connect(self.go_to_campus_nav_page)
+        self.study_planner_button.clicked.connect(self.go_to_study_planner_page)
+        self.notes_search_button.clicked.connect(self.go_to_notes_search_page) 
+        self.algo_info_button.clicked.connect(self.go_to_algo_info_page)
+        
 
-algo_info_btn = tk.Button(
-    nav_frame, 
-    text="ALGORITHM INFO", 
-    command=lambda: show_frame(algo_info_page),
-    **button_style
-)
-algo_info_btn.pack(fill='x', pady=10, padx=5)
+        # connecting functions to buttons on all pages
+        self.add_edge_button.clicked.connect(self.insert_edge)
+        self.run_algo_button.clicked.connect(self.handle_run_algorithm)
+
+    
+    # functions for sidebar buttons
+    def go_to_home_page(self):
+        self.stackedWidget.setCurrentWidget(self.home_page)
+
+    def go_to_campus_nav_page(self):
+        self.stackedWidget.setCurrentWidget(self.campus_nav) 
+
+    def go_to_study_planner_page(self):
+        self.stackedWidget.setCurrentWidget(self.study_planner)
+
+    def go_to_notes_search_page(self):
+        self.stackedWidget.setCurrentWidget(self.notes_search)
+
+    def go_to_algo_info_page(self):
+        self.stackedWidget.setCurrentWidget(self.algo_info)
+    
+    def insert_edge(self):
+        node1 = self.building_name_input.text()
+        node2 = self.neighbor_name_input.text()
+
+        if node1.lower() == node2.lower():
+            QMessageBox.warning(self, "Warning!", "Building cannot connect to itself")
+            return
+        
+        distance = self.distance_input.value()
+
+        if node1 not in adj_list:
+            adj_list[node1] = {}
+            self.update_combo_boxes(node1)
+
+        adj_list[node1][node2] = distance
+        
+        if node2 not in adj_list:
+            adj_list[node2] = {}
+            self.update_combo_boxes(node2)
+        
+        adj_list[node2][node1] = distance
+
+        self.building_name_input.setText("")
+        self.neighbor_name_input.setText("")
+        self.distance_input.setValue(0)
+        formatted_string = ""
+        for building, connection in adj_list.items():
+            formatted_string += (f"{building} : {connection}\n")
+        self.adjacency_list_display.setText(formatted_string)
+        print(adj_list)
+        
+
+    def update_combo_boxes(self, new_node):
+        if self.start_building_combo.findText(new_node) == -1:
+            self.start_building_combo.addItem(new_node)
+            self.end_building_combo.addItem(new_node)
+
+    def handle_run_algorithm(self):
+        start_node = self.start_building_combo.currentText()
+        end_node = self.end_building_combo.currentText()
+        algo = self.algorithm_combo.currentText()
+        
+        print(f"Running {algo} from {start_node} to {end_node}")
+        
+        self.navigator_output_display.setText(f"Algorithm running...\nStart: {start_node}\nEnd: {end_node}")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+
+    adj_list = {}
 
 
-page_container = tk.Frame(main_window)
-page_container.grid(row = 0, column = 1, sticky = 'nsew')
 
-page_container.rowconfigure(0, weight = 1)
-page_container.rowconfigure(0, weight = 1)
-
-
-campus_nav_page = tk.Frame(page_container)
-study_planner_page = tk.Frame(page_container)
-notes_search_engine_page = tk.Frame(page_container)
-algo_info_page = tk.Frame(page_container)
-
-for frame in (campus_nav_page, study_planner_page, notes_search_engine_page, algo_info_page):
-    frame.grid(row=0, column=0, sticky='nsew')
-
-# CAMPUS NAV PAGE
-campus_nav_label = tk.Label(campus_nav_page, text = "CAMPUS NAV PAGE")
-campus_nav_label.pack()
-
-# STUDY PLANNER PAGE
-study_planner_label = tk.Label(study_planner_page, text = "STUDY PLANNER PAGE")
-study_planner_label.pack()
-
-# NOTES SEARCH ENGINE PAGE
-notes_search_label = tk.Label(notes_search_engine_page, text = "NOTES SEARCH PAGE")
-notes_search_label.pack()
-
-
-# ALGO INFO PAGE
-algo_info_label = tk.Label(algo_info_page, text = "ALGO INFO PAGE")
-algo_info_label.pack()
-
-main_window.mainloop()
+    sys.exit(app.exec_())
